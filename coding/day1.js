79e63793b8979d7e6b36d40ad26c3ca240b56c02
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-06 20:56:09 +0530

    latest

--- coding/day1.js
+++ coding/day1.js
@@ -4,6 +4,18 @@ let y = 6
 let h = (arr) => {
   return  arr
 }
-
+let obj1 = {
+    name: "ojhal",
+    age: 25,
+    palace: "Rajasthan"
+  }
+  
+  function obj2(giveName){
+    this.name = giveName
+    
+    
+  }
+  let obj3 = new obj2("ojhal")
+  console.log(obj3)
 console.log((x < 10 || y > 1))
-console.log(h([1,2,3,4,5]))
+console.log(h([1,2,3]))
