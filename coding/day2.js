cc4c6cf1393fe263efeb07d02ce4663328225f04
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-07 23:05:19 +0530

    final

--- coding/day2.js
+++ coding/day2.js
@@ -8,3 +8,9 @@ function frankenSplice(arr1, arr2, n) {
   }
   
   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+  let abc = (arr) => {
+    let string = arr.slice(0,2);
+    return string 
+  }
+  console.log(abc([9,8,7]));
