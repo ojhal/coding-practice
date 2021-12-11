ab3dac76624a62125fe8cd7efbdd7ec89ab9b9ea
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-11 21:17:15 +0530

    latset 2

--- coding/day6.js
+++ coding/day6.js
@@ -28,3 +28,13 @@ function myFunction(num) {
   return num * 10;
 }
 console.log(newArr)
+
+function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    while (arr.length > 0) {
+      newArr.push(arr.splice(0, size));
+    }
+    return newArr;
+  }
+  
+  console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
