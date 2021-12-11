fea47bc1596f9311dcac566eef7f043cc7933f7f
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-11 20:14:23 +0530

    final

--- coding/day6.js
+++ coding/day6.js
@@ -7,3 +7,18 @@ var sum_odd_numbers = function () {
     return sum;
 }
 console.log(sum_odd_numbers());
+
+function largestNum(arr){
+    var largestNum = [0,0];
+    for (var i = 0; i < arr.length; i++){
+      for (var j = 0; j < arr[i].length; j++){
+        if(arr[i][j]>largestNum[i]){
+          largestNum[i] = arr[i][j]
+        }
+      }
+    }
+    return largestNum;
+  }
+  console.log(largestNum([[1,4],[19,45]]))
+  
+  
