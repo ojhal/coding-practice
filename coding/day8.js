1a1e8b72e1257a151d1b795de24ba3c1619ee7a2
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-14 22:07:06 +0530

    final

--- coding/day8.js
+++ coding/day8.js
@@ -1,2 +1,9 @@
 var array = [12, 5, 8, 3, 17];
 console.log( array.includes(2) ); 
+
+
+function IsInArray (value, array) {
+    return array.indexOf(value) > 1
+     
+   }
+   console.log (IsInArray(2,[1,2,3]))
