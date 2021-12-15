4541717ad20ed2dab506b6e2c29f4356c4277127
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-15 22:23:55 +0530

    final

--- coding/day9.js
+++ coding/day9.js
@@ -2,3 +2,9 @@ var array = [1,2,3,4,5];
 console.log(array.filter(function(el, ind){
   return ind % 2 === 0;
 }));
+
+const odds = arr.filter(number => {
+    return number % 2 !== 0;
+  });
+  
+  console.log(odds)
