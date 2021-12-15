fb244718547d7905f1a5961ad432808e2628f5e0
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-15 22:24:56 +0530

    latest odd num

--- coding/day9.js
+++ coding/day9.js
@@ -3,6 +3,9 @@ console.log(array.filter(function(el, ind){
   return ind % 2 === 0;
 }));
 
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const odds = arr.filter(number => {
     return number % 2 !== 0;
   });
