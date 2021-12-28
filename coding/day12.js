23bc52fa3bb0f7e986a93dc300dd3f6f9454ef80
Ojhal Patwa <patkarojhal26@gmail.com>
2021-12-28 20:22:42 +0530

    concate 2 array

--- coding/day12.js
+++ coding/day12.js
@@ -18,3 +18,8 @@ while (i--) {
 }
 
 console.log(e);
+
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+console.log(numbersCombined)
