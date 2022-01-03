e56259e605dc6c150a084dd6b3ea836120ae18b8
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-03 14:21:10 +0530

    map

--- coding/day17.js
+++ coding/day17.js
@@ -5,3 +5,11 @@ let filteredNumbers = numbers.map(function(num, index) {
      console.log(index)
   }
 })
+
+let numbers = [1, 2, 3, 4]
+let filteredNumbers = numbers.map(function(num, index) {
+  if (index < 3) {
+//      return num
+     console.log(num)
+  }
+})
