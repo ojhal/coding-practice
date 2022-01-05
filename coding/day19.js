2f7afca44749f2816da9b7ee2ae5d6f7d02c16a0
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-05 19:23:00 +0530

    includes

---
+++ coding/day19.js
@@ -0,0 +1,20 @@
+let arr = ['a', 'b', 'c']
+
+arr.includes('a', -100)
+arr.includes('b', -100) 
+arr.includes('c', -100)
+arr.includes('a', -2)
+console.log(arr.includes('a', -100))
+
+
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+
+
+console.log(pets.includes('at'));
