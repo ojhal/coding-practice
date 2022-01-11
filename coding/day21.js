40200b5f2b6fe3c88312aa9f9e4b1329fcaaf215
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-11 19:16:59 +0530

    loop

--- coding/day21.js
+++ coding/day21.js
@@ -1,3 +1,17 @@
 var a = "scaler"
 var result = a.substring(2,4)
 console.log(result);
+
+
+
+function pattern (){
+    var newRow = 4;
+    var newColumn = 7
+    for(var i=1; i<=newRow; i++){
+      for(var j=1; j<=newColumn; j++){
+        document.write ("*")
+      }
+     document.write("<br>")
+    }
+  }
+  pattern ();
