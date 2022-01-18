d5876bf843aebe16a2a995872c54efc81a12614b
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-18 11:02:54 +0530

    map

--- coding/day25.js
+++ coding/day25.js
@@ -12,3 +12,18 @@ numbers.forEach(function(value,index,array){
   forEachNumbers.push(value * 3)
 })
 console.log(forEachNumbers)
+
+
+var numbers = [3,2,4,5,7]
+var forEachNumbers = []
+numbers.forEach(function(value,index,array){
+//   console.log(value,index,array)
+  forEachNumbers.push(value * 3)
+})
+console.log(forEachNumbers)
+
+
+var doubleNumbers = numbers.map(function(value,index,array){
+  return(value * 4)
+})
+console.log(doubleNumbers)
