d7346a39e6d930542d9567d6d9a14c27830a1a55
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-18 14:07:35 +0530

    with condition

--- coding/day25.js
+++ coding/day25.js
@@ -34,3 +34,13 @@ for(let i=0; i<num.length; i++){//via for loop
   Mrnum.push(`Mr ${num[i]}`)
 }
 console.log(Mrnum)
+
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+var newNums = numbers.map((value)=>value*2)
+var newNumsFil = numbers.map((value)=>{
+  if(value % 2 === 0){
+    return value *2
+  }
+}).filter((value)=>!isNaN(value))//with condition
+console.log(newNums)
+console.log(newNumsFil)
