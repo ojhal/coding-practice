ed3cc3372d0276f8209c279adbb45a96f4f0a583
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-21 13:34:26 +0530

    primitives datatypes

---
+++ coding/day26.js
@@ -0,0 +1,17 @@
+//Premitive data types
+//String 
+let name = "ojhal"
+console.log(`daatatype ${(typeof name)}`)
+//Number
+let num = 7 
+console.log(`datatype ${(typeof num)}`)
+//Boolean
+let isRun = true 
+console.log(`datatype ${(typeof isRun)}`)
+//undefined
+let undef = undefined
+console.log(`datatype ${(typeof undef)}`)
+//Null
+let Null = null
+console.log(`datatype ${(typeof Null)}`)
+// its return object  but its premitive datatype
