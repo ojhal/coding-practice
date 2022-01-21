1af85aeca4f1032f4fa1b4ff756e23c4809bd491
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-21 13:58:12 +0530

    Refrence DataTypes

--- coding/day26.js
+++ coding/day26.js
@@ -15,3 +15,26 @@ console.log(`datatype ${(typeof undef)}`)
 let Null = null
 console.log(`datatype ${(typeof Null)}`)
 // its return object  but its premitive datatype
+
+
+//Refrence DataTypes
+//Array
+let myArray = [1,2,3,4,5,6,7]
+console.log(`datatype ${(typeof myArray)}`)
+//function
+function name(){
+  }
+console.log(`datatype ${(typeof name)}`)
+//Date
+ let date = new Date()
+console.log(`datatype ${(typeof  date)}`)
+//Objects litrals
+let obj = {
+  john: 34,
+  zen: 54,
+  ozz:64,
+  ojhal:74
+}
+ 
+console.log(`datatype ${(typeof obj)}`)
+console.log(obj)
