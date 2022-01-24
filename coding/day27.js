b401215bee6b13676dcafb36b31d0294b0e884e7
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-24 17:07:16 +0530

    The arguments object

--- coding/day27.js
+++ coding/day27.js
@@ -1,3 +1,10 @@
 var num = [5,34,67,87,01,0,23,12]
 num.sort()
 console.log(num)
+
+function func1(a, b, c) {
+    console.log(arguments[0],[1],[2])
+  
+  }
+  
+  func1("ojhal", 2, 3);//The arguments object
