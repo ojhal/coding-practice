5fcc2e4bbc7e6502f452531e52bfefd7d4df88fe
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-24 17:22:15 +0530

    using spread oprator

--- coding/day27.js
+++ coding/day27.js
@@ -8,3 +8,16 @@ function func1(a, b, c) {
   }
   
   func1("ojhal", 2, 3);//The arguments object
+
+  function func2(...arguments) {//using spread oprator
+    console.log(arguments[0])
+  
+  }
+  
+  func2("ojhalP", 5, 8);
+  
+  function foo(...args) {
+    console.log (args[1])
+  }
+  foo(1, 2, 3)
+  
