b29c0f0fa5677c75fb6a1b965126f6d03360edb9
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-28 12:32:26 +0530

    do/while loop

---
+++ coding/day29.js
@@ -0,0 +1,12 @@
+var num = [1,2,3,4,5]
+for (var i in num){
+  console.log(num[i])
+}
+
+console.log("*********//while and do while//********")
+var j = 0
+while (j<10){
+  console.log(j)
+  j++
+}
+  
