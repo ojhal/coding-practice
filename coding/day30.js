f9f739f874c647e9763861487ea71cff9efdfea1
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-30 22:16:38 +0530

    Render HTML Elements to the DOM

---
+++ coding/day30.js
@@ -0,0 +1,9 @@
+const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
+  //   Render HTML Elements to the DOM
+  
+  ReactDOM.render(JSX, document.getElementById('challenge-node'));
