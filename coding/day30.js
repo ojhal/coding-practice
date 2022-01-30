95dd394f0216a73e792266bf4bff18f306421222
Ojhal Patwa <patkarojhal26@gmail.com>
2022-01-30 22:20:52 +0530

    className camelcase

--- coding/day30.js
+++ coding/day30.js
@@ -7,3 +7,10 @@ const JSX = (
   //   Render HTML Elements to the DOM
   
   ReactDOM.render(JSX, document.getElementById('challenge-node'));
+
+//className camelcase
+  const JSX = (
+    <div className='myDiv'>
+      <h1>Add a class to this div</h1>
+    </div>
+  );
