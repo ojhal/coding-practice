3f5b777093f7bfe4e982acbac0d9cb7274ea7393
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-02 23:00:28 +0530

    Pass Props to a Stateless Functional Componen

--- coding/day33.js
+++ coding/day33.js
@@ -11,4 +11,29 @@ class MyComponent extends React.Component{
     }
   };
   ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
+  //Pass Props to a Stateless Functional Componen
+  const CurrentDate = (props) => {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <p>The current date is:{props.date} </p>
+        { /* Change code above this line */ }
+      </div>
+    );
+  };
   
+  class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          { /* Change code below this line */ }
+          <CurrentDate date={Date()}/>
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
