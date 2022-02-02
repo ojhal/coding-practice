9c11850b3d6627283521067c756eeb3155934c9d
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-02 11:02:32 +0530

    A React Component from Scratch

---
+++ coding/day33.js
@@ -0,0 +1,14 @@
+class MyComponent extends React.Component{
+    constructor(props){
+      super(props);
+    }
+    render(){
+      return(
+            <div id="challenge-node">
+                   <h1>My First React Component!</h1>
+            </div>
+      );
+    }
+  };
+  ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
+  
