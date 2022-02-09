1cb72dd51b7ab4807e9d478087efe762962e24fc
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-09 12:20:23 +0530

    Render State in the User Interface Another Way

--- coding/day38.js
+++ coding/day38.js
@@ -34,3 +34,25 @@ class StatefulComponent extends React.Component {
       );
     }
   };
+
+  //Render State in the User Interface Another Way
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'freeCodeCamp'
+      }
+    }
+    render() {
+      // Change code below this line
+      const name = this.state.name
+      // Change code above this line
+      return (
+        <div>
+          { /* Change code below this line */ }
+        <h1>{name}</h1>
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
