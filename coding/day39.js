6d3e4f55fbbb12953e780f8f2461e0b85ac739b4
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-10 19:23:12 +0530

    Set State with this.setState

--- coding/day39.js
+++ coding/day39.js
@@ -7,3 +7,29 @@ let obj ={
   //falsy value //nullish value
   //undefined.  //undefined
   //null.       //null
+
+//Set State with this.setState
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'Initial State'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      // Change code below this line
+  this.setState({
+    name: 'React Rocks!'
+  });
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click Me</button>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
