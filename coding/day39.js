6e8f956b8e0e044877d33ef0bef1404fe0248045
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-10 17:24:30 +0530

    nullish coalescing oprator ??

---
+++ coding/day39.js
@@ -0,0 +1,9 @@
+let obj ={
+    score: 10
+  }
+  let msg = `your score is ${obj.score ?? 100}`//nullish coalescing oprator ??
+  console.log(msg)
+  
+  //falsy value //nullish value
+  //undefined.  //undefined
+  //null.       //null
