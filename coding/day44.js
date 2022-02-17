20cf796d6e9098d1fb5116aa62343aaa419e09cf
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-17 23:20:06 +0530

    Use the Lifecycle Method componentDidMount

--- coding/day44.js
+++ coding/day44.js
@@ -72,3 +72,30 @@ class MyApp extends React.Component {
         )
     }
   };
+
+
+//Use the Lifecycle Method componentDidMount
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeUsers: null
+      };
+    }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          activeUsers: 1273
+        });
+      }, 3500);
+    }
+    render() {
+      return (
+        <div>
+          {/* Change code below this line */}
+          <h1>Active Users: { this.state.activeUsers }</h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
