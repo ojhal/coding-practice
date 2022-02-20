ec711b21ab27f18cf84f7946fb0afef15ef90a5b
Ojhal Patwa <patkarojhal26@gmail.com>
2022-02-20 19:10:41 +0530

    Add Inline Styles in React

--- coding/day46.js
+++ coding/day46.js
@@ -52,3 +52,18 @@ class OnlyEvens extends React.Component {
       );
     }
   };
+//Add Inline Styles in React
+  const styles ={
+    color: 'purple',
+    fontSize:40,
+    border: '2px solid purple'
+  }// Change code above this line
+  class Colorful extends React.Component {
+    render() {
+      // Change code below this line
+      return (
+        <div style={styles}>Style Me!</div>
+      );
+      // Change code above this line
+    }
+  };
