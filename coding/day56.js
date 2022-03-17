c863d0d210819ba76344d619ae10e843738655c2
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-17 12:29:34 +0530

    new array

--- coding/day56.js
+++ coding/day56.js
@@ -6,3 +6,14 @@ function avg(a,b,c){
 }
 let a = avg(...arr)//spread oprator
 console.log(a)
+
+
+const arr = [2,4,6]
+
+function avg(a,b,c){
+  return a+b+c/3
+  
+}
+// let a = avg(...arr)
+let a = [8,10,...arr,7,56]
+console.log(a)
