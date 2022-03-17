5797f105bd7bf79ffbd8d242dbfec0478c3a5a33
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-17 12:45:21 +0530

    concatinate

--- coding/day56.js
+++ coding/day56.js
@@ -17,3 +17,15 @@ function avg(a,b,c){
 // let a = avg(...arr)
 let a = [8,10,...arr,7,56]
 console.log(a)
+
+const arr = [2,4,6,67,80,00]
+
+function avg(a,b,c){
+  return a+b+c/3
+  
+}
+// let a = avg(...arr)
+// let a = [8,10,...arr,7,56]
+const a2 = [23,576,890,546]
+const a = [...a2, ...arr]
+console.log(a)
