24e05b5d0d5d54b9ae71399a90391190c2039f2a
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-17 12:50:00 +0530

    objects

--- coding/day56.js
+++ coding/day56.js
@@ -29,3 +29,21 @@ function avg(a,b,c){
 const a2 = [23,576,890,546]
 const a = [...a2, ...arr]
 console.log(a)
+//with objects
+const arr = [2,4,6,67,80,00]
+
+function avg(a,b,c){
+  return a+b+c/3
+  
+}
+// let a = avg(...arr)
+// let a = [8,10,...arr,7,56]
+// const a2 = [23,576,890,546]
+// const a = [...a2, ...arr]
+const obj = {
+  name: "john",
+  class: "1st year",
+  favLang:"javaScript"
+}
+const obj1 = {...obj,favLang: "react",name:"ojhal"}
+console.log(obj1)
