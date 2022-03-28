b2b4d973f71ce21d8f44e1f64156819c3aa88cc0
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-28 18:49:18 +0530

    Q2 L2

---
+++ coding/day59.js
@@ -0,0 +1,7 @@
+//leetcode Q2
+const nums = [3,2,3,8,6,7,7,7,7,7]
+var majorityElement = function(nums) {
+      return nums.sort((a, b) => a - b)[Math.floor(nums.length / 2)]
+    
+};
+console.log(nums.sort((a, b) => a - b)[Math.floor(nums.length / 2)])
