02598fc21bfae91819cc76aa78e13be7d030b49e
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-30 22:21:41 +0530

    l4

--- coding/day61.js
+++ coding/day61.js
@@ -11,3 +11,10 @@ var distributeCandies = function(candyType) {
     return Math.min(candyType.length/2, [...new Set(candyType)].length)
   
 };
+
+const  candyType =[1,1,2,2,3,3]
+var distributeCandies = function(candyType) {
+      return Math.min(candyType.length/2, [...new Set(candyType)].length)
+    
+};
+console.log(Math.min(candyType.length/2, [...new Set(candyType)].length))
