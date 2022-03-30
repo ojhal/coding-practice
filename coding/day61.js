73af8c1cccd4b6e7936bbf4a989dabf0784d0190
Ojhal Patwa <patkarojhal26@gmail.com>
2022-03-30 22:15:09 +0530

    Q4 l4

--- coding/day61.js
+++ coding/day61.js
@@ -3,3 +3,11 @@ const arr =[2,1,3]
 
 console.log(Math.min(...arr));
 //practise
+
+
+//Leetcode Q4
+
+var distributeCandies = function(candyType) {
+    return Math.min(candyType.length/2, [...new Set(candyType)].length)
+  
+};
