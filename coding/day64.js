cbbb222733012d8d12c9326f8044fb2784dbaf19
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-06 17:37:38 +0530

    callback function

--- coding/day64.js
+++ coding/day64.js
@@ -6,3 +6,13 @@ const func1 = () => {
   }
   func1()
   func2()
+  //using callback
+
+  const func3 = (friend,callback) => {
+    console.log(`use callback another function ${friend}`)
+    callback()
+  }
+  const func4 = () => {
+    console.log("use callback function")
+  }
+  func3("ojhal",func4)
