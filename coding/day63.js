b4fe814ebfe116cf7aacf091a5d50df5ac3a5eda
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-06 14:27:48 +0530

    promise

--- coding/day63.js
+++ coding/day63.js
@@ -19,4 +19,25 @@ function func1(){
   }).catch(function(){
      console.log('very bad bro')
   })
+  function func1(){
+    return new Promise(function(resolve,reject){
+          setTimeout(()=>{
+            const error = true;
+            if(!error){
+            console.log("promise has been resolved")
+            resolve()
+            }
+            else{
+              console.log("promise has not been resolved")
+              reject('sorry not fullfilled')
+            }
+          },2000);             
+    })
+   }
+  func1().then(function(){
+    console.log('thanks for resolving')
+  }).catch(function(error){
+     console.log('very bad bro Reason:'+ error)
+  })
+  
   
