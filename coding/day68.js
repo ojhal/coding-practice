a25aa8746fe8ca87f097e50a120ba885bf417400
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-13 13:19:19 +0530

    delete

--- coding/day68.js
+++ coding/day68.js
@@ -8,3 +8,12 @@ console.log("new set",mySet.size)
 console.log("new set",mySet.has(123))
 
 //has true/false
+
+const mySet = new Set(['john',123,"this",{a:4,b:8},"om"])
+console.log("new set",mySet.size)
+console.log("new set",mySet.has(123))
+
+console.log("befor remove", mySet.size)
+console.log(mySet.delete("john"))
+console.log("after remove", mySet.size)
+//delete
