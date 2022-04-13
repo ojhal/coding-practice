558b09a8427f1322a5ebe7c9e253acf17eb4f71b
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-13 13:07:50 +0530

    has method

--- coding/day68.js
+++ coding/day68.js
@@ -1,3 +1,10 @@
 const mySet = new Set(['john',123,"this",{a:4,b:8}])
 console.log("new set",mySet)
-//set
+//set// uniqe value
+
+
+const mySet = new Set(['john',123,"this",{a:4,b:8},"om"])
+console.log("new set",mySet.size)
+console.log("new set",mySet.has(123))
+
+//has true/false
