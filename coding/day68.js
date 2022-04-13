09a37ea240b43f05d2eda3bcf50eb7a58c01caff
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-13 13:24:06 +0530

    checking for has

--- coding/day68.js
+++ coding/day68.js
@@ -17,3 +17,16 @@ console.log("befor remove", mySet.size)
 console.log(mySet.delete("john"))
 console.log("after remove", mySet.size)
 //delete
+
+
+//for check has
+
+const mySet = new Set(['john',123,"this",{a:4,b:8},"om"])
+console.log("new set",mySet.size)
+console.log("new set",mySet.has(123))
+
+console.log("befor remove", mySet.size)
+console.log("befor remove", mySet.has("john"))
+console.log(mySet.delete("john"))
+console.log("after remove", mySet.size)
+console.log("befor remove", mySet.has("john"))
