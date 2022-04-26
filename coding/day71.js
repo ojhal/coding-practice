878f616fd9748cbf050cef1786d0e283767766a8
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-26 14:02:25 +0530

    with console

--- coding/day71.js
+++ coding/day71.js
@@ -9,3 +9,15 @@ var missingNumber = function(nums) {
     
 };
 //Q7 L7
+//with console
+var missingNumber = function(nums) {
+    let total1=0,total2=0
+    for(let i=0;i<nums.length;i++){
+        total1+=nums[i]
+        total2+=i
+    }
+    total2+=nums.length
+    return total2-total1
+    
+};
+console.log(missingNumber([3,0,1]))
