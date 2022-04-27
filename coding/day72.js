62dd0b72f5fcb0206f4d4c4f9a5d52e4ad3ce707
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-27 14:12:42 +0530

    final

--- coding/day72.js
+++ coding/day72.js
@@ -8,3 +8,16 @@ const outerFun = (a) =>{
     innerFunc()
   }
   outerFun(5)
+
+
+const outerFun = (a) =>{
+    let b = 20
+    const innerFunc = () =>{
+      let sum = a+b
+      console.log(`the sum of ${sum}`)
+    }
+    return innerFunc;
+  }
+  let inner = outerFun(5)
+  console.log(inner)
+  inner()
