8e6dd04562631ef4e7a20761672f7464bc68caf0
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-28 13:49:53 +0530

    for of/ in loops

---
+++ coding/day74.js
@@ -0,0 +1,18 @@
+//for of loop
+
+const arr = ['rahul','pooja','jay','deep']
+for (let value in arr){
+  console.log(value)
+}
+
+//for in loop
+
+const arr1 = {
+  name:"ojhal",
+  age:25,
+  education:"m.sc",
+  job: "SDE"
+}
+for (let i in arr1){
+  console.log(i)
+}
