d537800c2c58be410325b248e02db6cc08f0ea5a
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-28 13:50:48 +0530

    changes

--- coding/day74.js
+++ coding/day74.js
@@ -1,12 +1,12 @@
 //for of loop
-
+//itrate on array
 const arr = ['rahul','pooja','jay','deep']
 for (let value in arr){
   console.log(value)
 }
 
 //for in loop
-
+//itrate on objects
 const arr1 = {
   name:"ojhal",
   age:25,
