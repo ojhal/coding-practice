f29c90284b6df778761d406c89b35afff505752d
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-28 12:18:34 +0530

    final

--- coding/day73.js
+++ coding/day73.js
@@ -13,3 +13,18 @@ output.push(arr);
 
 flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
 //L8
+
+flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
+
+var flipImage = (image) => {
+  let ans = []
+  for (let val of image){
+    let arr = val.reverse()
+    for(let i =0; i<arr.length; i++){
+      arr[i]=arr[i] === 1?0:1
+    }
+    ans.push(arr)
+  }
+  console.log(ans)
+}
+flipImage([[0,0,1],[1,0,1],[0,0,0]])//calling
