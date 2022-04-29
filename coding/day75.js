3536e593723147bafe0d28da80b070d45f0dba76
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-29 14:12:21 +0530

    moveZeroes

---
+++ coding/day75.js
@@ -0,0 +1,13 @@
+var moveZeroes = function(nums) {
+    let head = 0
+ for(let i = 0; i < nums.length; i++) {
+   console.log(nums[i])
+   if(nums[i] !== 0) {
+     [nums[head], nums[i]] = [nums[i], nums[head]]
+     head++
+   }
+//     console.log(head++)
+ }
+};
+moveZeroes([0,1,0,3,12])
+//L9
