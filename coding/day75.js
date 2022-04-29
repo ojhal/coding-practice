7e1746d01aafe6eecea638ec69918428ed967169
Ojhal Patwa <patkarojhal26@gmail.com>
2022-04-29 14:15:07 +0530

    value getting in console

--- coding/day75.js
+++ coding/day75.js
@@ -11,3 +11,16 @@ var moveZeroes = function(nums) {
 };
 moveZeroes([0,1,0,3,12])
 //L9
+
+
+var moveZeroes = function(nums) {
+    let head = 0
+ for(let i = 0; i < nums.length; i++) {
+   console.log([nums[head], nums[i]] = [nums[i], nums[head]])
+   if(nums[i] !== 0) {
+     [nums[head], nums[i]] = [nums[i], nums[head]]
+     head++
+   }
+ }
+};
+moveZeroes([0,1,0,3,12])
