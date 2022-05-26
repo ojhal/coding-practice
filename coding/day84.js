a8e2d4c92c4e0a78cf6974d7afda0df524d8f1c0
Ojhal Patwa <patkarojhal26@gmail.com>
2022-05-26 16:11:14 +0530

    _.map

--- coding/day84.js
+++ coding/day84.js
@@ -3,3 +3,13 @@ var result = _.sum(array)
 console.log(result)//sum
 
 //add dependency "lodash" on your codesandbox etc..
+
+
+
+//_.map
+var square = (n) => {
+    return n * n;
+  }
+   
+  var result = _.map([4, 8], square);
+  console.log(result)
