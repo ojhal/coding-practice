5dcd31f46b824d80d457f87f8226e6a883ff2e51
Ojhal Patwa <patkarojhal26@gmail.com>
2022-05-30 23:10:39 +0530

    object DS

--- coding/day85.js
+++ coding/day85.js
@@ -7,3 +7,23 @@ console.log(catNames)
 const animalsName = ['cat','dog']
 const [catName , dogName] = animalsName
 console.log(catName)
+
+
+//object
+ const dog = {
+   name: 'jinni',
+   age: 5,
+   breed: 'german'
+ }
+ const {name,age,breed} = dog
+ console.log(name)
+ console.log(breed)
+ //rest op
+ const cat = {
+   name: 'lola',
+   age: 2,
+   breed:'ragdoll'
+ }
+ const {name, ...rest}=cat
+ console.log(rest)
+  console.log(name)
