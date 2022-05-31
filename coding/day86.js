11fd33705d872b9b978d65b83c995044d89a4376
Ojhal Patwa <patkarojhal26@gmail.com>
2022-05-31 18:04:58 +0530

    change the name of the veriable

---
+++ coding/day86.js
@@ -0,0 +1,8 @@
+const dog = {
+    name: 'rex',
+    age:5,
+    breed: 'german'
+  }
+   const {name:dogName,age:dogAge,breed:dogBreed}=dog
+   console.log(dogName)
+   //change the name of the veriable
