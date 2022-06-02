2ad247bfa1af43e3547c0d7dfb91811a8f0df6e3
Ojhal Patwa <patkarojhal26@gmail.com>
2022-06-02 10:18:57 +0530

    interview quetion

--- coding/day87.js
+++ coding/day87.js
@@ -4,3 +4,17 @@ const[first,[sec,third],fourth] = nestArr
 console.log(first)
 console.log(fourth)
 console.log(sec)
+
+
+
+//interview quetion
+console.log(1);
+setTimeout(
+function(){
+  console.log(2)
+},1000);
+setTimeout(
+function(){
+  console.log(3)
+},0);
+console.log(4);
