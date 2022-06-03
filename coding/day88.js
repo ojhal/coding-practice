75e9ba36e21c2f38e766c863d7838d96b23046f2
Ojhal Patwa <patkarojhal26@gmail.com>
2022-06-03 10:30:50 +0530

    Handle an Action in the Store

--- coding/day88.js
+++ coding/day88.js
@@ -24,3 +24,27 @@ let action={
   
   // Dispatch the action here:
   store.dispatch(loginAction());
+
+  const defaultState = {
+    login: false
+  };
+  
+  const reducer = (state = defaultState, action) => {
+    // Change code below this line
+  if (action.type === "LOGIN") {
+      return {
+        login: true
+      };
+    } else {
+      return state;
+    }
+    // Change code above this line
+  };
+  
+  const store = Redux.createStore(reducer);
+  
+  const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
