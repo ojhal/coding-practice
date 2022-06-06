bc450cb6aecd55c6bf69a9d662ebdbe48eebc110
Ojhal Patwa <patkarojhal26@gmail.com>
2022-06-06 22:41:31 +0530

    Use a Switch Statement to Handle Multiple Actions

--- coding/day88.js
+++ coding/day88.js
@@ -48,3 +48,41 @@ let action={
       type: 'LOGIN'
     }
   };
+//Use a Switch Statement to Handle Multiple Actions
+
+  const defaultState = {
+    authenticated: false
+  };
+  
+  const authReducer = (state = defaultState, action) => {
+    // Change code below this line
+  switch (action.type) {
+      case "LOGIN":
+        return {
+          authenticated: true
+        };
+  
+      case "LOGOUT":
+        return {
+          authenticated: false
+        };
+  
+      default:
+        return defaultState;
+    }
+    // Change code above this line
+  };
+  
+  const store = Redux.createStore(authReducer);
+  
+  const loginUser = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+  
+  const logoutUser = () => {
+    return {
+      type: 'LOGOUT'
+    }
+  };
