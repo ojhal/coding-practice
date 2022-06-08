d1cb30f00bfb2a3b11101b390c4d31091da0c414
Ojhal Patwa <patkarojhal26@gmail.com>
2022-06-08 11:11:17 +0530

    switch/case

---
+++ coding/day89.js
@@ -0,0 +1,22 @@
+const day = 6
+switch(day){
+    case 0:
+    console.log("today is monday")
+    break;
+    case 1:
+    console.log("today is tuesday")
+    break;
+    case 2:
+    console.log("today is wednesday")
+     case 4: 
+     console.log("today is thursday")
+    break;
+     case 5:
+    console.log("today is friday")
+    break;
+     case 6:
+     console.log("today is saturday")
+    break;
+  default:
+    console.log("enter the valid day")
+}
