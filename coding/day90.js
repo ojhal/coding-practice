b8a44d5e1f074628c96b311375c72ab601a273f4
Ojhal Patwa <patkarojhal26@gmail.com>
2022-06-10 12:02:44 +0530

    for practice

---
+++ coding/day90.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Text, Button } from "react-native";
+
+const App = () => {
+  let [counter, setCounter] = useState("0/10");
+  return (
+    <>
+      <Button
+        title="counter"
+        onPress={() => {
+          if (counter < 10) {
+            setCounter((prev) => prev + 1);
+          } else {
+            setCounter(0);
+          }
+        }}
+      />
+      <Text>{counter}</Text>
+    </>
+  );
+};
+
+export default App;
+//for practice
