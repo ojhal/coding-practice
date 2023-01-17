bd460a2ad2b46407ecc5af980e4d87902ab2b45d
Ojhal Patwa <patkarojhal26@gmail.com>
2023-01-17 16:28:40 +0530

    Promise API

--- coding/Morden.js
+++ coding/Morden.js
@@ -640,3 +640,40 @@ p1.then(()=>{
 p1.then(()=>{
   console.log("hurray")
 })
+
+
+//Promise API
+
+let p1 = new Promise((resolve,reject) =>{
+  setTimeout(()=>{
+    resolve("value1")
+  },1000)
+})
+
+let p2 = new Promise((resolve,reject) =>{
+  setTimeout(()=>{
+    resolve("value2")
+  },2000)
+})
+let p3 = new Promise((resolve,reject) =>{
+  setTimeout(()=>{
+    resolve("value3")
+  },3000)
+})
+
+// p1.then((value)=>{
+//   console.log(value)
+// })
+
+
+// p2.then((value)=>{
+//   console.log(value)
+// })
+
+// p3.then((value)=>{
+//   console.log(value)
+// })
+let promise_all = Promise.all([p1,p2,p3])
+promise_all.then((value)=>{
+  console.log(value)
+})
