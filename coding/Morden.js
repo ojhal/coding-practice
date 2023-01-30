f1d43ace022f81bb74ed2540307ad2c29ad481b4
Ojhal Patwa <patkarojhal26@gmail.com>
2023-01-30 12:02:31 +0530

    lexical scoping

--- coding/Morden.js
+++ coding/Morden.js
@@ -997,3 +997,25 @@ function init() {
 }
 let a = init();
 a()
+// eg lexical scoping
+function returnFunc() {
+  const x = () => {
+    let a = 1
+    console.log(a)
+    const y = () => {
+      // let a = 2
+      console.log(a)
+      const z = () => {
+        // let a = 3
+        console.log(a)
+      }
+      z()
+    }
+    a = 999
+    y()
+  }
+  return x
+}
+
+let a = returnFunc()
+a()
